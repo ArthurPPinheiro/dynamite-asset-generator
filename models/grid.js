03efde5007d754d2430824a0a55b96c8ba548735
Arthur Pinheiro <arthurppinheiro@hotmail.com>
2025-05-26 19:45:51 -0300

    implemented image resizing and refactored grid generator

--- models/grid.js
+++ models/grid.js
@@ -1,5 +1,6 @@
 class Grid {
-    constructor({ title, ctaText, altText, titleFr, ctaTextFr, altTextFr, catId, width, alignment, color, image, assetType, date }) {
+    constructor({ title, ctaText, altText, titleFr, ctaTextFr, altTextFr, catId, width, alignment, color, image, imageFile, assetType, date }) {
+        this.date = this.generateDate(date);
         this.title = title;
         this.ctaText = ctaText;
         this.altText = altText;
@@ -10,19 +11,65 @@ class Grid {
         this.width = width;
         this.alignment = alignment;
         this.color = color;
-        this.image = image;
-        this.cleanTitle = Grid.cleanString(title);
-        this.date = Grid.formatDate(date);
+        this.imageFile = imageFile;
+        this.cleanTitle = this.cleanString(title);
+        this.image = this.getImagePath();
+        this.formattedDate = this.cleanString(this.getDateMonthDay());
     }
 
-    static cleanString(string) {
+    generateDate(date){
+        const dateArray = date.split('-');
+        const year = dateArray[0];
+        const month = (dateArray[1] - 1);
+        const day = dateArray[2];
+        return new Date(year, month, day);
+    }
+
+    getMonthName() {
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        return monthNames[this.date.getMonth()];
+    }
+
+    cleanString(string) {
         return string.trim()
             .replace(/\s+/g, '')
             .replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase();
     }
-    
-    static formatDate(date) {
-        return date.replace(/\//g, '-').replace(/\s+/g, '').toLowerCase();
+
+    getDateMonthDay() {
+        const month = this.cleanString(this.getMonthName());
+        const day = this.date.getDate().toString().padStart(2, '0');
+        return `${month}${day}`;
+    }
+
+    getAssetOutputDirectoryPath() {
+        return 'outputs/' + this.getDateMonthDay() + '-' + this.cleanTitle;
+    }
+
+    getImageOutputDirectoryPath() {
+        return 'outputs/' + this.getDateMonthDay() + '-' + this.cleanTitle + '/images/';
+    }
+
+    getFilePath() {
+        return 'outputs/' + this.getDateMonthDay() + '-' + this.cleanTitle + '/dyn-grid-' + this.cleanTitle + '-' + this.getDateMonthDay() + '-' + this.date.getFullYear() + '.html';
+    }
+
+    getFilePathFrench() {
+        return 'outputs/' + this.getDateMonthDay() + '-' + this.cleanTitle + '/dyn-grid-' + this.cleanTitle + '-' + this.getDateMonthDay() + '-' + this.date.getFullYear() + '-fr.html';
+    }
+
+    getFileName() {
+        return `Grids-${this.getDateMonthDay()}-${this.cleanTitle}-Img`;
+    }
+
+    getImagePath() {
+        const monthName = this.getMonthName();
+        const monthNumber = (this.date.getMonth() + 1).toString().padStart(2, '0');
+
+        return `${monthNumber}-${monthName}/${this.getFileName()}`;
     }
 }
 
--- routes/grid.js
+++ routes/grid.js
@@ -1,57 +1,138 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const handlebars = require('express-handlebars').create();
-const router = express.Router();
 const Grid = require('../models/Grid');
+const sharp = require('sharp');
+const multer = require('multer');
 
-router.post('/', (req, res) => {
+const router = express.Router();
+const upload = multer({ dest: 'uploads/' })
+
+router.post('/', upload.single('imageFile'), async (req, res) => {
+    const file = req.file;
+
     const grid = new Grid(req.body);
 
+    await compressImages(grid, file);
+
+    await genereateAsset(grid);
+
+    res.redirect('/');
+});
+
+async function genereateAsset(grid) {
     const templatePath = path.join(__dirname, `../templates/grid.hbs`);
-    const templateSrc = fs.readFileSync(templatePath, 'utf-8');
+    const templateSrc = await fs.readFile(templatePath, { encoding: 'utf-8' }); 
     const template = handlebars.handlebars.compile(templateSrc);
 
     const year = new Date().getFullYear();
 
     const output = template({ ...grid });
 
-    grid.title = grid.titleFr;
-    grid.ctaText = grid.titleFr;
-    grid.altText = grid.altTextFr;
+    const frenchData = {
+        ...grid,
+        title: grid.titleFr,
+        ctaText: grid.ctaTextFr, 
+        altText: grid.altTextFr,
+    };
 
-    const outputFr = template({ ...grid });
+    const outputFr = template({ ...frenchData });
 
-    fs.mkdir('outputs/' + grid.date + '-' + grid.cleanTitle, { recursive: true }, (err) => {
-        if (err) {
-            console.error('Error creating directory:', err);
-        } else {
-            console.log('Directory created successfully!');
+    await fs.mkdir(grid.getAssetOutputDirectoryPath(), { recursive: true });
+
+    await fs.writeFile(grid.getFilePath(), output);
+
+    await fs.writeFile(grid.getFilePathFrench(), outputFr);
+}
+
+async function compressImages(grid, file) {
+    if(!file || !file.path){
+        console.error('Invalid file or file path');
+        return;
+    }
+
+    await fs.mkdir(grid.getImageOutputDirectoryPath(), { recursive: true });
+
+    const imageTasks = [
+        { width: 1200, height: 900, quality: 75, format: 'jpeg', maxSize: 70, isMobile: false },
+        { width: 1200, height: 900, quality: 75, format: 'webp', maxSize: 70, isMobile: false },
+        { width: 600, height: 450, quality: 75, format: 'jpeg', maxSize: 50, isMobile: true },
+        { width: 600, height: 450, quality: 75, format: 'webp', maxSize: 50, isMobile: true },
+    ]
+
+    for(const task of imageTasks){
+        await proccessImage(grid, file, task.width, task.height, task.quality, task.maxSize, task.format, task.isMobile);
+    }
+
+    console.log('Image processing tasks completed successfully');
+}
+
+async function proccessImage(grid, file, width, height, quality, max_size, format, isMobile = false){
+    if(!file || !file.path){
+        console.error('Invalid file or file path');
+        return;
+    }
+
+    let currentQuality = quality;
+    const minQuality = 10;
+    const qualityStep = 5;
+    let processedBuffer;
+    let processedInfo;
+    const maxAttempts = 20;
+
+    let sharpInstance = sharp(file.path).resize(width, height);
+    for(let attempt = 1; attempt <= maxAttempts; attempt++){
+
+        if (format === 'jpeg') {
+            sharpInstance = sharpInstance.jpeg({ quality: currentQuality, progressive: true, optimizeScans: true });
+        } 
+        else if (format === 'webp') {
+            sharpInstance = sharpInstance.webp({ quality: currentQuality });
         }
 
+        try {
+            const output = await sharpInstance.toBuffer({ resolveWithObject: true });
+            const currentSizeKB = output.info.size / 1000;
 
-        var filePath = 'outputs/' + grid.date + '-' + grid.cleanTitle + '/dyn-grid-' + grid.cleanTitle + '-' + grid.date + '-' + year + '.html';
-        var filePathFr = 'outputs/' + grid.date + '-' + grid.cleanTitle + '/dyn-grid-' + grid.cleanTitle + '-' + grid.date + '-' + year + '-fr.html';
+            console.log(`Attempt ${attempt}: Quality ${currentQuality || 'N/A'}, Size: ${currentSizeKB.toFixed(2)}KB`);
 
-        fs.writeFile(filePath, output, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            } else {
-                console.log('Data written to file successfully!');
+            if (currentSizeKB <= max_size) {
+                processedBuffer = output.data;
+                processedInfo = output.info;
+                console.log('Image size is within the acceptable limit.');
+                break; 
             }
-        });
 
-        fs.writeFile(filePathFr, outputFr, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
+            if (currentQuality > minQuality) {
+                currentQuality -= qualityStep;
+                if (currentQuality < minQuality) currentQuality = minQuality;
             } else {
-                console.log('Data written to file successfully!');
+                processedBuffer = output.data;
+                processedInfo = output.info;
+                console.warn(`Could not reduce size further for ${format} or min quality reached. Final attempt size: ${currentSizeKB.toFixed(2)}KB`);
+                break;
             }
-        });
+        } catch (err) {
+            console.error(`Error during sharp processing attempt ${attempt} for ${format}:`, err);
+            return null;
+        }
     }
-    );
-
-    res.redirect('/');
-});
-
+    
+    if (processedBuffer) {
+        const finalFileName = grid.getFileName() + (isMobile ? '-Mobile' : '') + '.' + format;
+        const finalFilePath = path.join(grid.getImageOutputDirectoryPath(), finalFileName);
+        try {
+            await fs.writeFile(finalFilePath, processedBuffer); 
+            console.log(`Image processed and saved as ${finalFilePath}. Final size: ${(processedInfo.size / 1000).toFixed(2)}KB`);
+            return finalFilePath;
+        } catch (err) {
+            console.error('Error writing final image to file:', err);
+            return null;
+        }
+    } else {
+        console.error(`No processed buffer to save for ${format}${isMobile ? ' (Mobile)' : ''}. Original file: ${file.originalname}`);
+        return null;
+    }
+}
 module.exports = router;
